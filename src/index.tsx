61fd4ac0f3906021be8e8791d8664f195b56c49f
Eulália Ribeiro <eulalia.ribeiro@lais.huol.ufrn.br>
2021-06-22 23:39:26 -0300

    dia 1: preparando o ambiente de desenvolvimento

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import './services/firebase'
+
+// DOM = Document Object Model
 
 ReactDOM.render(
+  // método render: renderizar/exibir um elemento dentro de um HTML
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// HTML dentro do JS => JSX, aqui no caso é TSX
